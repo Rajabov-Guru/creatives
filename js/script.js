24db502bc177c7ff42445e3033afb81c21afa2b6
Afros <71890231+SocialBugGG@users.noreply.github.com>
2022-01-15 13:24:18 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+const swiper = new Swiper('.text-slider', {
+    // Optional parameters
+
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+});
+const swiper2 = new Swiper('.blog-slider', {
+    // Optional parameters
+
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+});
+const swiper3 = new Swiper('.member-slider', {
+    // Optional parameters
+
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+});
